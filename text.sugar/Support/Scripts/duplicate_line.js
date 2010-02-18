d647401ead663a66fb405a1dd424d03a35f4d71f
Maurice Kühlborn <mk@icheb.(none)>
2010-02-18 11:48:37 +0100

    Changed Duplicate Line to Duplicate Selection

--- text.sugar/Support/Scripts/duplicate_line.js
+++ text.sugar/Support/Scripts/duplicate_line.js
@@ -7,15 +7,18 @@ var Range = require('range').Range;
 // exports.main is your primary function, run automatically by Spice
 exports.main = function() {
 	// save the current cursor position for later
-	var currentpos = textContext.getFirstSelection().location;
-	// Grab the range of the current line
-	var linerange = textContext.rangeForLine();
-	// copy the line to duplicate
-	var linecontent = linerange.string();
+	var currentSelection = textContext.getFirstSelection();
+	var currentPos = currentSelection.limit;
+	// check if nothing is selected
+	if(currentSelection.length == 0) {
+		var currentSelection = textContext.rangeForLine();
+	}
+	// copy the string to duplicate
+	var selectionContent = currentSelection.string();
 	// duplicate line
-	if (new TextRecipe().replace(linecontent+linecontent,linerange).apply()) {
+	if (new TextRecipe().replace(selectionContent+selectionContent,currentSelection).apply()) {
 		// reset cursor positon
-		textContext.setSelection(new Range(currentpos, 0));
+		textContext.setSelection(new Range(currentPos, 0));
 		return true;
 	} else {
 		return false;
